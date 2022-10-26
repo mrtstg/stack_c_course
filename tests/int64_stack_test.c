2b98e3ee17432a4a7989ebdd4fe23802e1febf91
mrtstg <russiankurwacsgo@gmail.com>
2022-10-26 16:12:18 +0300

    added tests for checking correct values extract

--- tests/int64_stack_test.c
+++ tests/int64_stack_test.c
@@ -1,12 +1,24 @@
 #include "../unity/unity.h"
 #include "../lib/int64_stack.h"
 #include "../lib/random_gens.h"
+#include <stdio.h>
 
 void setUp() {
     init_srand();
 }
 
 void tearDown() {}
+
+void test_stack_count_function(void) {
+    int64_t stack_size = random_in_range(10, 100);
+    int64_t stack_elems_amount = random_in_range(1, 9);
+    struct Int64Stack stack = Int64Stack_create((size_t)stack_size);
+    for (size_t i = 0; i < stack_elems_amount; i++) {
+        int64_t random_num = random_in_range(0, 10000);
+        TEST_ASSERT(Int64Stack_push(&stack, random_num));
+    }
+    TEST_ASSERT(Int64Stack_count(&stack) == stack_elems_amount);
+}
 
 void test_stack_full_function(void) {
     struct Int64Stack stack = Int64Stack_create(5);
@@ -38,8 +50,35 @@ void test_stack_empty_function_on_used_stack(void) {
     TEST_ASSERT(Int64Stack_is_empty(&stack));
 }
 
+void test_correct_values_pop(void) {
+    int64_t nums_amount = random_in_range(10, 100);
+    struct Int64Stack stack = Int64Stack_create(100);
+    struct Int64Array random_nums_array = Int64Array_create(nums_amount);
+    for (size_t i = 0; i < nums_amount; i++) {
+        int64_t random_num = random_in_range(0, 10000);
+        TEST_ASSERT(Int64Array_set(&random_nums_array, i, random_num));
+        TEST_ASSERT(Int64Stack_push(&stack, random_num));
+    }
+
+    for (size_t i = 0; i < nums_amount; i++) {
+        struct Int64Result res = Int64Stack_pop(&stack);
+        struct Int64Result arr_res = Int64Array_get(&random_nums_array, nums_amount - i - 1);
+        TEST_ASSERT(res.valid);
+        TEST_ASSERT(arr_res.valid);
+        TEST_ASSERT(arr_res.value == res.value);
+    }
+
+    TEST_ASSERT(Int64Stack_is_empty(&stack));
+    struct Int64Result res = Int64Stack_pop(&stack);
+    TEST_ASSERT(!res.valid);
+}
+
 int main(void) {
     UNITY_BEGIN();
+    RUN_TEST(test_stack_count_function);
     RUN_TEST(test_stack_full_function);
+    RUN_TEST(test_stack_empty_function_on_empty_stack);
+    RUN_TEST(test_stack_empty_function_on_used_stack);
+    RUN_TEST(test_correct_values_pop);
     return UNITY_END();
 }
